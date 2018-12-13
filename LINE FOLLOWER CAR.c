70a254083b4fac4f66363f64ad8291031a1e3529
TechGuyAPC <43348853+TechGuyAPC@users.noreply.github.com>
2018-12-13 11:07:07 -0500

    Update LINE FOLLOWER CAR.c

--- LINE FOLLOWER CAR.c
+++ LINE FOLLOWER CAR.c
@@ -1,15 +1,15 @@
 
 task main()
 {
- untilBump(Touch);
-	startMotor(RightMotor, 88);
-	startMotor(LeftMotor,  -63);
-	untilSonarLessThan(10, Input);
-	stopMotor(RightMotor);
-	stopMotor(LeftMotor);
+ untilBump(bumpSwitch);
+	startMotor(rightMotor, 80);
+	startMotor(leftMotor,  80); //reversed in motor config
+	untilSonarLessThan(10, sonar); //once it reaches the wall @ 10 inches
+	stopMotor(rightMotor);
+	stopMotor(leftMotor);
 		wait(5);
-	startMotor(RightMotor,-70);
-	startMotor(LeftMotor,80);
+	startMotor(rightMotor,-70);
+	startMotor(leftMotor,-70); //reversed in motor config
     wait(5);
   stopMotor(RightMotor);
   stopMotor(LeftMotor);
