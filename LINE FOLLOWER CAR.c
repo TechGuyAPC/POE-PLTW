c903d7590f850676917ad707d000a1017c8a4bfa
TechGuyAPC <43348853+TechGuyAPC@users.noreply.github.com>
2018-12-13 10:51:45 -0500

    Update LINE FOLLOWER CAR.c

--- LINE FOLLOWER CAR.c
+++ LINE FOLLOWER CAR.c
@@ -1,6 +1,18 @@
 
 task main()
 {
+ untilBump(Touch);
+	startMotor(RightMotor, 88);
+	startMotor(LeftMotor,  -63);
+	untilSonarLessThan(10, Input);
+	stopMotor(RightMotor);
+	stopMotor(LeftMotor);
+		wait(5);
+	startMotor(RightMotor,-70);
+	startMotor(LeftMotor,80);
+    wait(5);
+  stopMotor(RightMotor);
+  stopMotor(LeftMotor);
 
 
 
